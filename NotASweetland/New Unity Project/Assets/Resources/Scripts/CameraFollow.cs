adf22be8273a04043f293e48e3910b3f5e364d73
Siitoo <alfonsosanchez1896@gmail.com>
2019-07-07 11:44:50 +0200

    First iteration camera and some changes in assets folder

---
+++ NotASweetland/New Unity Project/Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Transform player_transform = null;
+    private float player_speed = 1.0f;
+
+    public float radius = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_transform = GameObject.FindGameObjectWithTag("Player").transform;
+        player_speed = player_transform.gameObject.GetComponent<PlayerMovement>().movement_speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 direction = player_transform.position - transform.position;
+        direction.y = 0;
+
+        if (direction.magnitude > radius )
+        {
+            direction = direction.normalized * player_speed * Time.deltaTime;
+            transform.position += direction;
+        }
+
+        transform.LookAt(player_transform);
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+    }
+
+}
---
+++ NotASweetland/New Unity Project/Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movement_speed = 1.0f;
+    public float rotation_speed = 5.0f;
+    public bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Mathf.Abs(Input.GetAxis("LJoystickHorizontal")) > 0.1f || Mathf.Abs(Input.GetAxis("LJoystickVertical")) > 0.1f)
+        {
+            moving = true;
+        }
+        else
+            moving = false;
+
+        transform.Translate(new Vector3((Input.GetAxis("LJoystickHorizontal") * movement_speed * Time.deltaTime), 0, (Input.GetAxis("LJoystickVertical") * movement_speed * Time.deltaTime)));
+        transform.RotateAround(transform.position, new Vector3(0, 1, 0), Input.GetAxis("RJoystickHorizontal") * rotation_speed * Time.deltaTime);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 n = new Vector3(0, 0.6f, -1);
+
+        n *= 10f/2;
+        
+        Gizmos.DrawLine(transform.position,-transform.forward);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.forward);
+    }
+}
