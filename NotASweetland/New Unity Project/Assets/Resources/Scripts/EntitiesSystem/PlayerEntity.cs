94e8aaa4bf694700389cafdaf820c3357d80220d
Siitoo <alfonsosanchez1896@gmail.com>
2019-07-14 22:08:47 +0200

    More movement

--- NotASweetland/New Unity Project/Assets/Resources/Scripts/EntitiesSystem/PlayerEntity.cs
+++ NotASweetland/New Unity Project/Assets/Resources/Scripts/EntitiesSystem/PlayerEntity.cs
@@ -33,11 +33,20 @@ public class PlayerEntity : Entity
 
             if (Mathf.Abs(Input.GetAxis("LJoystickHorizontal")) >= dead_zone || Mathf.Abs(Input.GetAxis("LJoystickVertical")) >= dead_zone)
             {
-                float horizontal = Input.GetAxis("LJoystickHorizontal") * Time.deltaTime;
-                float vertical = Input.GetAxis("LJoystickVertical") * Time.deltaTime;
-                float angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+                /* float horizontal = Input.GetAxis("LJoystickHorizontal") * Time.deltaTime;
+                 float vertical = Input.GetAxis("LJoystickVertical") * Time.deltaTime;
+                 float angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
 
-                transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+                 transform.rotation = Quaternion.Euler(0.0f, Mathf.Clamp(angle, -10f, max_rot_velocity), 0.0f);*/
+
+                Vector3 new_pos = new Vector3( transform.position.x + Input.GetAxis("LJoystickHorizontal")*movement_speed, transform.position.y, transform.position.z + Input.GetAxis("LJoystickVertical") * movement_speed);
+                float target_angle = Mathf.Atan2(Input.GetAxis("LJoystickHorizontal"), Input.GetAxis("LJoystickVertical")) * Mathf.Rad2Deg;
+                float current = Mathf.Atan2(transform.forward.x, transform.forward.z) * Mathf.Rad2Deg;
+
+                float delta = Mathf.DeltaAngle(target_angle, current);
+                //delta = Mathf.Clamp(-delta, -2000.0f, 2000.0f);
+
+                transform.rotation *= Quaternion.AngleAxis(-delta * Time.deltaTime* 2, Vector3.up);
 
 
                 movement = transform.forward * movement_speed;
