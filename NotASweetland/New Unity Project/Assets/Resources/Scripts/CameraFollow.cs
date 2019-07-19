0c7a71ebf6f3e7f337da5f060deec7dda982e0f9
Acaree <acampamar@gmail.com>
2019-07-19 17:35:15 +0200

    Movement fixed!

---
+++ NotASweetland/New Unity Project/Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float dead_zone = 0.1f;
+
+    public float rotate_speed = 1f;
+
+    [HideInInspector]
+    public bool fixed_camera = false;
+
+    private GameObject player = null;
+    private Vector3 offset = Vector3.zero;
+
+    private float horizontal = 0f;
+    private float vertical = 0f;
+
+    public Transform pivot = null;
+
+    public float min_rotation_camera_x = 45f;
+    public float max_rotation_camera_x = 180f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = player.transform.position - transform.position;
+
+        pivot.transform.position = player.transform.position;
+        pivot.transform.parent = player.transform;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (Mathf.Abs(Input.GetAxis("RJoystickHorizontal")) >= dead_zone || Mathf.Abs(Input.GetAxis("RJoystickVertical")) >= dead_zone)
+        {
+
+            horizontal = Input.GetAxis("RJoystickHorizontal") * rotate_speed;
+            pivot.transform.Rotate(0, -horizontal, 0);
+            vertical = Input.GetAxis("RJoystickVertical") * rotate_speed;
+            pivot.transform.Rotate(-vertical, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > min_rotation_camera_x && pivot.rotation.eulerAngles.x < max_rotation_camera_x)
+        {
+            pivot.rotation = Quaternion.Euler(min_rotation_camera_x, pivot.eulerAngles.y, pivot.eulerAngles.z);
+        }
+
+        if (pivot.rotation.eulerAngles.x > max_rotation_camera_x && pivot.rotation.eulerAngles.x < (360f - min_rotation_camera_x))
+        {
+            pivot.rotation = Quaternion.Euler((360f - min_rotation_camera_x), pivot.eulerAngles.y, pivot.eulerAngles.z);
+        }
+
+        Quaternion q = Quaternion.Euler(pivot.transform.eulerAngles.x, pivot.transform.eulerAngles.y, 0);
+        transform.position = pivot.transform.position - (q * offset);
+
+        if (transform.position.y < pivot.transform.position.y)
+            transform.position = new Vector3(transform.position.x, pivot.transform.position.y - 0.5f, transform.position.z);
+
+
+
+
+        transform.LookAt(player.transform);
+    }
+}
---
+++ NotASweetland/New Unity Project/Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movement_speed = 1.0f;
+    public float jump_force = 300.0f;
+    public float rot_speed = 10f;
+    public bool moving = false;
+    private bool jumping = false; 
+
+    private Animator anim = null;
+    Vector3 new_dir = Vector3.zero;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input_joystic = new Vector2(Input.GetAxis("LJoystickHorizontal"), Input.GetAxis("LJoystickVertical"));
+
+        if (Input.GetButtonDown("R1"))
+        {
+            anim.SetBool("Preparing_arm", true);
+            anim.Play("Preparing_arm");
+            Time.timeScale = 0.25f;
+        }
+
+        else if (Input.GetButtonUp("R1"))
+        {
+            anim.SetBool("Preparing_arm", false);
+            Time.timeScale = 1.0f;
+        }
+
+        if (Input.GetAxis("R2") > 0.5f)
+        {
+            anim.SetBool("Preparing_arm", false);
+            anim.SetBool("Throwing_arm", true);
+            anim.Play("Throwing_arm");
+        }
+
+        else
+        {
+            anim.SetBool("Throwing_arm", false);
+        }
+
+
+
+        if (Input.GetButtonDown("AButton") && !jumping)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3 (0.0f, jump_force, 0.0f));
+            anim.SetBool("jump", true);
+            anim.Play("Jumping");
+            jumping = true;
+        }
+
+        if (!anim.GetBool("Throwing_arm"))
+        {
+
+            if (Mathf.Abs(input_joystic.x) > 0 || Mathf.Abs(input_joystic.y) > 0) 
+            {
+                moving = true;
+                if (!jumping)
+                {
+                    anim.SetBool("walk", true);
+                    anim.Play("Run");
+                }
+
+                transform.Rotate(new Vector3(0, 1, 0), input_joystic.x * Time.deltaTime * rot_speed * 10);
+
+                if(input_joystic.y > 0)
+                transform.position += (new Vector3(0, 0, input_joystic.y) * movement_speed * Time.deltaTime).magnitude * transform.forward;
+                else
+                    transform.position += (new Vector3(0, 0, input_joystic.y) * movement_speed/3 * Time.deltaTime).magnitude * (-transform.forward);
+            }
+
+            else
+            {
+                moving = false;
+            }
+        }
+
+        else
+        {
+
+        }
+
+        if (!jumping && !anim.GetBool("Preparing_arm") && !anim.GetBool("Throwing_arm") && !moving)
+        {
+            anim.SetBool("walk", false);
+            anim.Play("Idle");
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        jumping = false;
+    }
+
+}
+
+
